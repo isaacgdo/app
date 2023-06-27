1efd016f541adb3a8c03acf39626ce081574662c
Isaac Oliveira <isaacgdoliveira@gmail.com>
2023-06-27 01:14:30 -0300

    remove unused struct

--- producer-service/models/models.go
+++ producer-service/models/models.go
@@ -2,30 +2,6 @@ package models
 
 import "time"
 
-type SearchData struct {
-	NextPageToken string `json:"nextPageToken"`
-	RegionCode    string `json:"regionCode"`
-	PageInfo      struct {
-		TotalResults   int `json:"totalResults"`
-		ResultsPerPage int `json:"resultsPerPage"`
-	} `json:"pageInfo"`
-	Items []struct {
-		ID struct {
-			Kind    string `json:"kind"`
-			VideoID string `json:"videoId"`
-		} `json:"id"`
-		Snippet struct {
-			PublishedAt          time.Time `json:"publishedAt"`
-			ChannelID            string    `json:"channelId"`
-			Title                string    `json:"title"`
-			Description          string    `json:"description"`
-			ChannelTitle         string    `json:"channelTitle"`
-			LiveBroadcastContent string    `json:"liveBroadcastContent"`
-			PublishTime          time.Time `json:"publishTime"`
-		} `json:"snippet"`
-	} `json:"items"`
-}
-
 type CommentItemData struct {
 	ID      string `json:"id"`
 	Snippet struct {
